48c99bbade0d3726f8e37672886aa74e4dc4610a
cvhabitat <wangshidong128@gmail.com>
2021-06-02 20:40:23 +0100

    Update plot_cm.py

--- utils/plot_cm.py
+++ utils/plot_cm.py
@@ -7,22 +7,8 @@ from sklearn import svm, datasets
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
 from sklearn.utils.multiclass import unique_labels
-'''
-# import some data to play with
-iris = datasets.load_iris()
-X = iris.data
-y = iris.target
-class_names = iris.target_names
 
-# Split the data into a training set and a test set
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
-
-# Run classifier, using a model that is too regularized (C too low) to see
-# the impact on the results
-classifier = svm.SVC(kernel='linear', C=0.01)
-y_pred = classifier.fit(X_train, y_train).predict(X_test)
-'''
-log_eval = '../../exp/aid/02/log_eval/'
+log_eval = '../../exp/AID/20percent/log_eval/'
 y_pred = open(log_eval + 'predictions.txt', 'r')
 y_pred = np.asarray(y_pred.readlines(), dtype=int)
 
